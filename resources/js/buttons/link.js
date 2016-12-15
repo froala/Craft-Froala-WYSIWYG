60273a2e23b6de33e776fa4cb18d7f971ca19476
Bert Oost <bert@oostdesign.com>
2016-12-15 21:12:17 +0100

    Added support for link edit via Craft's entry link modal

--- resources/js/buttons/link.js
+++ resources/js/buttons/link.js
@@ -16,7 +16,7 @@ $.FroalaEditor.RegisterCommand('insertLinkEntry', {
         this.selection.save();
 
         var modal = Craft.createElementSelectorModal('Entry', {
-            criteria: {locale: currentLocale},
+            criteria: { locale: currentLocale },
             onSelect: $.proxy(function (elements) {
                 if (elements.length) {
 
@@ -25,6 +25,32 @@ $.FroalaEditor.RegisterCommand('insertLinkEntry', {
                     var urlLabel = _selectedText || element.label;
 
                     _editor.link.insert(url, urlLabel);
+
+                    return true;
+                }
+            }, this),
+            closeOtherModals: false
+        });
+    }
+});
+
+$.FroalaEditor.RegisterCommand('linkEdit', {
+    callback: function (cmd, val) {
+        var currentLocale = (Craft.getLocalStorage('BaseElementIndex.locale') || Craft.locale),
+            _editor = this;
+
+        var $current_link = this.link.get();
+
+        var modal = Craft.createElementSelectorModal('Entry', {
+            criteria: { locale: currentLocale },
+            onSelect: $.proxy(function (elements) {
+                if (elements.length) {
+
+                    var element = elements[0];
+                    var url = element.url + '#entry:' + element.id;
+
+                    $current_link = $($current_link);
+                    $current_link.attr('href', url);
 
                     return true;
                 }
