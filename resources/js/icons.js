0abc9a3f7c9a76914f1e6b7b021431a84725ce8c
Bert Oost <bert@oostdesign.com>
2018-05-16 22:15:59 +0200

    Removed old JS integration files

--- resources/js/icons.js
+++
@@ -1,3 +0,0 @@
-$.FroalaEditor.DefineIcon('insertLinkEntry', { NAME: 'link' });
-$.FroalaEditor.DefineIcon('insertAssetImage', { NAME: 'image' });
-$.FroalaEditor.DefineIcon('insertAssetFile', { NAME: 'file-o' });
