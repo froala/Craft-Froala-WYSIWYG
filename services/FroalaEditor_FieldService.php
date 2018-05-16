a0c722bb2bac5fdc5e45469d75bb769cb3ff4778
Bert Oost <bert@oostdesign.com>
2018-05-16 22:16:47 +0200

    Updated JS integration for the Froala field (compared against Craft 3 version)

---
+++ services/FroalaEditor_FieldService.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace Craft;
+
+/**
+ * Class FroalaEditor_FieldService
+ */
+class FroalaEditor_FieldService extends BaseApplicationComponent
+{
+    const CORE_PLUGINS = [
+        'bold'            => 'Bold',
+        'italic'          => 'Italic',
+        'underline'       => 'Underline',
+        'strikeTrough'    => 'Strike Through',
+        'subscript'       => 'Subscript',
+        'superscript'     => 'Superscript',
+        'outdent'         => 'Outdent',
+        'indent'          => 'Indent',
+        'undo'            => 'Undo',
+        'redo'            => 'Redo',
+        'insertHR'        => 'Insert HR',
+        'clearFormatting' => 'Clear Formatting',
+        'selectAll'       => 'Select All',
+    ];
+
+    /**
+     * @var FroalaEditorPlugin
+     */
+    protected $plugin;
+
+    /**
+     * Return the plugin instance
+     *
+     * @return FroalaEditorPlugin
+     */
+    public function getPlugin()
+    {
+        if (empty($this->plugin)) {
+
+            $this->plugin = craft()->plugins->getPlugin('froalaeditor');
+        }
+
+        return $this->plugin;
+    }
+
+    /**
+     * Returns a list with all possible editor plugins
+     *
+     * @return array
+     * @throws \CException
+     */
+    public function getEditorPlugins()
+    {
+        $editorPlugins = $this->getPlugin()->getEditorPlugins();
+        $pluginsEnabled = $this->getPlugin()->getSettings()->getAttribute('enabledPlugins');
+        if (!empty($pluginsEnabled) && is_array($pluginsEnabled)) {
+            foreach ($editorPlugins as $pluginName => $pluginLabel) {
+                if (!in_array($pluginName, $pluginsEnabled)) {
+                    unset($editorPlugins[$pluginName]);
+                }
+            }
+        }
+
+        return $editorPlugins;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTransforms()
+    {
+        $allTransforms = craft()->assetTransforms->getAllTransforms();
+        $transformList = [];
+
+        foreach ($allTransforms as $transform) {
+            $transformList[] = [
+                'handle' => HtmlHelper::encode($transform->handle),
+                'name' => HtmlHelper::encode($transform->name)
+            ];
+        }
+
+        return $transformList;
+    }
+
+    /**
+     * @param int    $folderSourceId
+     * @param string $folderSubPath
+     * @param bool   $createDynamicFolders
+     * @return string
+     * @throws \Exception
+     * @throws InvalidSubpathException
+     * @throws InvalidSourceException
+     */
+    public function determineFolderId($folderSourceId, $folderSubPath, $createDynamicFolders = true)
+    {
+        try {
+
+            $folderId = $this->resolveSourcePathToFolderId($folderSourceId, $folderSubPath, $createDynamicFolders);
+            $folderId = 'folder:' . $folderId . ':single';
+
+        } catch (InvalidSubpathException $e) {
+
+            // If this is a new element, the sub path probably just contained a token that returned null, like {id}
+            // so use the user's upload folder instead
+            if (empty($this->element->id) || !$createDynamicFolders) {
+
+                $userModel = craft()->userSession->getUser();
+                $userFolder = craft()->assets->getUserFolder($userModel);
+                $folderName = 'field_' . $this->model->id;
+
+                $folder = craft()->assets->findFolder([
+                    'parentId' => $userFolder->id,
+                    'name'     => $folderName
+                ]);
+
+                if ($folder) {
+                    $folderId = $folder->id;
+                } else {
+                    $folderId = $this->createSubFolder($userFolder, $folderName);
+                }
+
+                IOHelper::ensureFolderExists(craft()->path->getAssetsTempSourcePath() . $folderName);
+            } else {
+                // Existing element, so this is just a bad subpath
+                throw $e;
+            }
+        }
+
+        return $folderId;
+    }
+
+    /**
+     * @param      $sourceId
+     * @param      $subPath
+     * @param bool $createDynamicFolders
+     * @return int
+     * @throws InvalidSourceException
+     * @throws InvalidSubpathException
+     */
+    private function resolveSourcePathToFolderId($sourceId, $subPath, $createDynamicFolders = true)
+    {
+        // Get the root folder in the source
+        $rootFolder = craft()->assets->getRootFolderBySourceId($sourceId);
+
+        // Make sure the root folder actually exists
+        if (!$rootFolder) {
+            throw new InvalidSourceException();
+        }
+
+        // Are we looking for a sub folder?
+        $subPath = is_string($subPath) ? trim($subPath, '/') : '';
+
+        if (strlen($subPath) === 0) {
+            $folder = $rootFolder;
+        } else {
+            // Prepare the path by parsing tokens and normalizing slashes.
+            try {
+                $renderedSubPath = craft()->templates->renderObjectTemplate($subPath, $this->element);
+            } catch (\Exception $e) {
+                throw new InvalidSubpathException($subPath);
+            }
+
+            // Did any of the tokens return null?
+            if (
+                strlen($renderedSubPath) === 0 ||
+                trim($renderedSubPath, '/') != $renderedSubPath ||
+                strpos($renderedSubPath, '//') !== false
+            ) {
+                throw new InvalidSubpathException($subPath);
+            }
+
+            $subPath = IOHelper::cleanPath($renderedSubPath, craft()->config->get('convertFilenamesToAscii'));
+
+            $folder = craft()->assets->findFolder([
+                'sourceId' => $sourceId,
+                'path'     => $subPath . '/'
+            ]);
+
+            // Ensure that the folder exists
+            if (!$folder) {
+                if (!$createDynamicFolders) {
+                    throw new InvalidSubpathException($subPath);
+                }
+
+                // Start at the root, and, go over each folder in the path and create it if it's missing.
+                $parentFolder = $rootFolder;
+
+                $segments = explode('/', $subPath);
+
+                foreach ($segments as $segment) {
+                    $folder = craft()->assets->findFolder([
+                        'parentId' => $parentFolder->id,
+                        'name'     => $segment
+                    ]);
+
+                    // Create it if it doesn't exist
+                    if (!$folder) {
+                        $folderId = $this->createSubFolder($parentFolder, $segment);
+                        $folder = craft()->assets->getFolderById($folderId);
+                    }
+
+                    // In case there's another segment after this...
+                    $parentFolder = $folder;
+                }
+            }
+        }
+
+        return $folder->id;
+    }
+
+    /**
+     * @param AssetFolderModel $currentFolder
+     * @param string           $folderName
+     * @return integer
+     */
+    private function createSubFolder(AssetFolderModel $currentFolder, $folderName)
+    {
+        $response = craft()->assets->createFolder($currentFolder->id, $folderName);
+
+        if ($response->isError() || $response->isConflict()) {
+            // If folder doesn't exist in DB, but we can't create it, it probably exists on the server.
+            $newFolder = new AssetFolderModel(
+                [
+                    'parentId' => $currentFolder->id,
+                    'name'     => $folderName,
+                    'sourceId' => $currentFolder->sourceId,
+                    'path'     => ($currentFolder->parentId ? $currentFolder->path . $folderName : $folderName) . '/'
+                ]
+            );
+            $folderId = craft()->assets->storeFolder($newFolder);
+
+            return $folderId;
+        } else {
+
+            $folderId = $response->getDataItem('folderId');
+
+            return $folderId;
+        }
+    }
+}
